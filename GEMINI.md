dc074bb050244ce83a53519265eb3b122ad38abe
Oskar Okuno <2677573+MrOggy85@users.noreply.github.com>
2025-07-04 15:27:01 +0900

    init

---
+++ GEMINI.md
@@ -0,0 +1,7 @@
+
+
+
+You don't need to run `npm run dev` because I am already running it from a different shell.
+
+## Gemini Added Memories
+- The user wants to build an alternative Slack frontend. The left column will show a list of channels, and the center column will be a single chronological feed of all messages from all channels.
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="left-column">
+        <button>Button 1</button>
+        <button>Button 2</button>
+        <button>Button 3</button>
+      </div>
+      <div className="center-column">
+        {/* Main content will go here */}
+      </div>
+      <div className="right-column">
+        right
+      </div>
+    </>
+  )
+}
+
+export default App
